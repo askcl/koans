74d43c23bfd472cbbf41303a61e24814499278ec
Askcl <askcl@mail.ru>
2016-08-31 14:51:56 +0300

    267

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,11 +13,11 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :chanel
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
   end
-
   # WRITE CODE HERE
 end
 
